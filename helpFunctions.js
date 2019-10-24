49324a0ad17eb6643ddbab4f72a96f198fb3040c
Sava Vukosavljev <Sava.Vukosavljev@p3-holding.com>
2019-10-24 16:41:48 +0200

    Add style and refactor initGrid.

--- helpFunctions.js
+++ helpFunctions.js
@@ -1,13 +1,43 @@
-const makeField = (x, y, color = 'white') => (
-  new fabric.Rect({
+const UNIT = 40;
+
+function initGrid() {
+  canvas.clear();
+
+  for (let i = 1; i < 10; i++) {
+    canvas.add(new fabric.Line([i * UNIT, 0, i * UNIT, 400], propsForLine()));
+    canvas.add(new fabric.Line([0, i * UNIT, 400, i * UNIT], propsForLine()));
+  }
+}
+
+function initSquares() {
+  allSquares = [];
+
+  for (let i = 0; i < 10; i++) {
+    allSquares.push([])
+    for (let j = 0; j < 10; j++) {
+      allSquares[i].push(
+        {
+          selected: false,
+          marked: false,
+          canvas: makeField(i * 40, j * 40),
+          coordinate: [i * UNIT, j * UNIT]
+        }
+      );
+      canvas.add(allSquares[i][j].canvas);
+    }
+  }
+}
+
+const makeField = (x, y, color = 'white') => {
+  return new fabric.Rect({
     left: x,
     top: y,
     fill: color,
-    width: UNIT - 0.5,
-    height: UNIT - 0.5,
+    width: UNIT,
+    height: UNIT,
     selectable: false,
   })
-)
+}
 
 const showFreeFields = (x, y) => (
   [
@@ -44,14 +74,21 @@ function selectFieldAndMarkOthers(el, x, y) {
   el.set('fill', '#ccc');
   allSquares[x][y].selected = true;
 
-  // select surrounding fields
-  const filteredFields = showFreeFields(x, y)
-    .filter(([x, y]) => x > -1 && y > -1 && x < 10 && y < 10 && !allSquares[x][y].selected)
-  console.log(filteredFields.length)
-  if (filteredFields.length === 0) {
-    console.log('GAME OVER');
+  const surroundingFields = showFreeFields(x, y)
+    .filter(([x, y]) => x > -1 && y > -1 && x < 10 && y < 10 && !allSquares[x][y].selected);
+
+  if (surroundingFields.length === 0) {
+    GAME_OVER = true;
     return;
   }
 
-  filteredFields.forEach(([x, y]) => markFields(x, y))
+  surroundingFields.forEach(([x, y]) => markFields(x, y));
+}
+
+function propsForLine() {
+  return {
+    stroke: 'black',
+    strokeWidth: 1,
+    selectable: false,
+  }
 }
--- initialization.js
+++ initialization.js
@@ -1,42 +1,65 @@
 fabric.Object.prototype.selectable = false;
 const canvas = new fabric.Canvas('canvasId');
-const allSquares = [];
-const UNIT = 40;
+let allSquares = [];
+
 let FIRST_TIME = true;
+let GAME_OVER = false;
 
-// Init grid
-for (let i = 0; i <= 400; i += UNIT) {
-  canvas.add(new fabric.Line([i, 0, i, 400], propsForLine()));
+initGrid();
+initSquares();
+registeClickEvent();
+
+document.getElementById('newGame').addEventListener('click', () => {
+  FIRST_TIME = true;
+  GAME_OVER = false;
+  initGrid();
+  initSquares();
+  registeClickEvent();
+})
+
+
+function registeClickEvent() {
+  canvas.on('mouse:down', function (event) {
+    const element = event.target;
+    const coorX = element.left / UNIT;
+    const coorY = element.top / UNIT;
+    if (FIRST_TIME) {
+      selectFieldAndMarkOthers(element, coorX, coorY);
+      FIRST_TIME = false;
+      return;
+    }
+
+    if (allSquares[coorX][coorY].marked) {
+      removeMarks(coorX, coorY);
+      selectFieldAndMarkOthers(element, coorX, coorY);
+    }
+  });
+
+  canvas.renderAll();
 }
-// canvas.add(new fabric.Line([399, 0, 399, 400], propsForLine()));
 
-for (let i = 0; i <= 400; i += UNIT) {
-  canvas.add(new fabric.Line([0, i, 400, i], propsForLine()));
-}
-// canvas.add(new fabric.Line([0, 399, 400, 399], propsForLine()));
+//   canvas.off('mouse:down')
 
-function propsForLine() {
-  return {
-    stroke: 'black',
-    strokeWidth: 1,
-    selectable: false,
-  }
-}
+//   showFreeFields(coorX, coorY)
+//     .filter(([x, y]) => x > -1 && y > -1 && x < 10 && y < 10)
+//     .forEach(([x, y]) => {
+//       allSquares[x][y] = makeField(x * UNIT, y * UNIT, 'pink');
+//       allSquares[x][y].on('mouseover', e => {
+//         e.target.set('fill', 'yellow');
+//         canvas.add(allSquares[x][y]);
+//         canvas.renderAll();
+//       });
 
-// init squares
-for (let i = 0; i < 10; i++) {
-  allSquares.push([])
-  for (let j = 0; j < 10; j++) {
-    allSquares[i].push(
-      {
-        selected: false,
-        marked: false,
-        canvas: makeField(i * 40, j * 40),
-        coordinate: [i * UNIT, j * UNIT]
-      }
-    );
-    canvas.add(allSquares[i][j].canvas);
-  }
-}
+//       allSquares[x][y].on('mouseout', e => {
+//         e.target.set('fill', 'pink')
+//         canvas.renderAll();
+//       });
 
-canvas.renderAll();
+//       allSquares[x][y].on('mousedown', e => {
+//         allSquares[x][y].off('mouseout');
+//         allSquares[x][y].off('mouseoover');
+//         e.target.set('fill', 'blue');
+//         allSquares[x][y].selected = true;
+//         // reset fields
+//         canvas.renderAll();
+//       })
