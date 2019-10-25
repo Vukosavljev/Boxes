233d0aa829c88d725d873ccd07f4508f1f3f736e
Sava Vukosavljev <Sava.Vukosavljev@p3-holding.com>
2019-10-25 16:27:35 +0200

    Add es lint.

--- initialization.js
+++ initialization.js
@@ -10,7 +10,6 @@ document.getElementById('yes').addEventListener('click', () => {
 });
 
 let ALL_SQUARES = [];
-let showGameOver = false;
 let FIRST_TIME = true;
 
 initGame()
@@ -28,7 +27,7 @@ function registeClickEvent() {
     }
 
     if (ALL_SQUARES[coorX][coorY].marked) {
-      removeMarks(coorX, coorY);
+      removeMarks();
       selectFieldAndMarkOthers(element, coorX, coorY);
     }
   });
@@ -52,4 +51,106 @@ function setColor(e, color) {
       canvas.renderAll();
     }
   }
+}
+
+const UNIT = 40;
+
+function initGrid() {
+  canvas.clear();
+
+  for (let i = 1; i < 10; i++) {
+    canvas.add(new fabric.Line([i * UNIT, 0, i * UNIT, 400], propsForLine()));
+    canvas.add(new fabric.Line([0, i * UNIT, 400, i * UNIT], propsForLine()));
+  }
+}
+
+function initGame() {
+  FIRST_TIME = true;
+  initGrid();
+  ALL_SQUARES = [];
+
+  for (let i = 0; i < 10; i++) {
+    ALL_SQUARES.push([])
+    for (let j = 0; j < 10; j++) {
+      ALL_SQUARES[i].push(
+        {
+          selected: false,
+          marked: false,
+          canvas: makeField(i * 40, j * 40),
+          coordinate: [i * UNIT, j * UNIT]
+        }
+      );
+      canvas.add(ALL_SQUARES[i][j].canvas);
+    }
+  }
+  registeClickEvent();
+}
+
+const makeField = (x, y, color = 'white') => {
+  return new fabric.Rect({
+    left: x,
+    top: y,
+    fill: color,
+    width: UNIT,
+    height: UNIT,
+    selectable: false,
+  })
+}
+
+const showFreeFields = (x, y) => (
+  [
+    [x, y - 3],
+    [x + 2, y - 2],
+    [x + 3, y],
+    [x + 2, y + 2],
+    [x, y + 3],
+    [x - 2, y + 2],
+    [x - 3, y],
+    [x - 2, y - 2]
+  ]
+)
+
+function markFields(x, y) {
+  if (!ALL_SQUARES[x][y].selected) {
+    ALL_SQUARES[x][y].canvas.set('fill', 'pink');
+    ALL_SQUARES[x][y].marked = true;
+  }
+}
+
+function removeMarks() {
+  ALL_SQUARES.forEach(block => {
+    block.forEach(square => {
+      if (square.marked && !square.selected) {
+        square.canvas.set('fill', 'white');
+        square.marked = false;
+      }
+    })
+  })
+}
+
+function selectFieldAndMarkOthers(el, x, y) {
+  el.set('fill', '#ccc');
+  ALL_SQUARES[x][y].selected = true;
+
+  const surroundingFields = showFreeFields(x, y)
+    .filter(([x, y]) => x > -1 && y > -1 && x < 10 && y < 10 && !ALL_SQUARES[x][y].selected);
+
+  if (surroundingFields.length === 0) {
+    modal.style.display = 'block';
+    return;
+  }
+
+  surroundingFields.forEach(([x, y]) => markFields(x, y));
+}
+
+function propsForLine() {
+  return {
+    stroke: 'black',
+    strokeWidth: 1,
+    selectable: false,
+  }
+}
+
+function closeModal() {
+  modal.style.display = 'none';
 }
